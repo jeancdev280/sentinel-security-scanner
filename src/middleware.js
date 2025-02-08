8d11858d2c58dc59ce2a9df9d18827b3fa45f050
jeancdev280 <jpclinux11@gmail.com>
2025-02-08 13:31:03 -0500

    Add files via upload

---
+++ src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "34b36901-8c97-4f8c-a1c0-aa2a9b3f4e78");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
